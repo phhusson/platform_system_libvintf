e8b7d95b884d3c5c86f0b9c8c4cf7b1f13ed7cac
Yifan Hong <elsk@google.com>
2017-04-04 15:44:26 -0700

    Hide CompatibiilityMatrix::findKernel and getSepolicy
    as part of the effort to minimize public API for libvintf.
    
    Bug: 34620633
    Test: pass
    Change-Id: I0218c317ceae48be45f7c54d29ebf0c702c23150

--- include/vintf/CompatibilityMatrix.h
+++ include/vintf/CompatibilityMatrix.h
@@ -32,20 +32,20 @@ namespace vintf {
 struct CompatibilityMatrix {
     constexpr static Version kVersion{1, 0};
 
-    // Find a MatrixKernel entry that has version v. nullptr if not found.
-    const MatrixKernel *findKernel(const KernelVersion &v) const;
-
-    const Sepolicy &getSepolicy() const;
-
 private:
     bool add(MatrixHal &&hal);
     bool add(MatrixKernel &&kernel);
     void clear();
+
+    // Find a MatrixKernel entry that has version v. nullptr if not found.
+    const MatrixKernel *findKernel(const KernelVersion &v) const;
+
     // Return an iterable to all MatrixHal objects. Call it as follows:
     // for (const MatrixHal &e : cm.getHals()) { }
     ConstMapValueIterable<std::string, MatrixHal> getHals() const;
 
     friend struct HalManifest;
+    friend struct RuntimeInfo;
     friend struct CompatibilityMatrixConverter;
     friend struct LibVintfTest;
 
