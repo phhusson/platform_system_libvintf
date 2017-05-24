0fd7aeff56fc919dd70aeddfa10df39b4ca7e9a7
Yifan Hong <elsk@google.com>
2017-05-24 14:37:19 -0700

    HalGroup is the base class for HalManifest and CompatibilityMatrix.
    Move common functions to HalGroup.h
    
    Test: libvintf_test
    Test: vintf_object_test
    Bug: 38217107
    Change-Id: I8acac6f4fcf79e2c9c3ec9df475f341c772aa983

---
+++ include/vintf/HalGroup.h
@@ -0,0 +1,59 @@
+
+#ifndef ANDROID_VINTF_HAL_GROUP_H
+#define ANDROID_VINTF_HAL_GROUP_H
+
+#include <map>
+
+#include "MapValueIterator.h"
+
+namespace android {
+namespace vintf {
+
+// A HalGroup is a wrapped multimap from name to Hal.
+// Hal.getName() must return a string indicating the name.
+template <typename Hal>
+struct HalGroup {
+   public:
+    virtual ~HalGroup() {}
+
+    // Add an hal to this HalGroup so that it can be constructed programatically.
+    bool add(Hal&& hal) {
+        if (!shouldAdd(hal)) {
+            return false;
+        }
+        mHals.emplace(hal.getName(), std::move(hal));  // always succeed
+        return true;
+    }
+
+   protected:
+    // sorted map from component name to the component.
+    // The component name looks like: android.hardware.foo
+    std::multimap<std::string, Hal> mHals;
+
+    // override this to filter for add.
+    virtual bool shouldAdd(const Hal&) const { return true; }
+
+    // Return an iterable to all ManifestHal objects. Call it as follows:
+    // for (const auto& e : vm.getHals()) { }
+    ConstMultiMapValueIterable<std::string, Hal> getHals() const {
+        return ConstMultiMapValueIterable<std::string, Hal>(mHals);
+    }
+
+    // Get any HAL component based on the component name. Return any one
+    // if multiple. Return nullptr if the component does not exist. This is only
+    // for creating objects programatically.
+    // The component name looks like:
+    // android.hardware.foo
+    Hal* getAnyHal(const std::string& name) {
+        auto it = mHals.find(name);
+        if (it == mHals.end()) {
+            return nullptr;
+        }
+        return &(it->second);
+    }
+};
+
+}  // namespace vintf
+}  // namespace android
+
+#endif  // ANDROID_VINTF_HAL_GROUP_H
